7b0076f5752c17187ff54cdd95c941bae28c97dc
Gabriel Cardinali <gabrielccardinalif@gmail.com>
2021-08-10 15:03:31 -0300

    create a API including MVC project

---
+++ CursoAPI/Models/Context.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=CursoMVC;User Id=SA;Password=Ai!3129494;");
+        }
+        public DbSet<Categoria> Categorias { get; set; }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
